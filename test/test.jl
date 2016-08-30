d7f4e4999f5147e469a9fee5abf1d4474d778c60
Robert DJ <git@dahl-jacobsen.dk>
2016-08-30 18:05:55 +0200

    Simplifying tests

--- test/test.jl
+++ test/test.jl
@@ -5,73 +5,29 @@ eps = 1e-5
 m = 4
 sigma = 2.0
 
-# 1D
-begin
-  N = 128
-  x = collect( linspace(-0.4, 0.4, N) )
-  fHat = collect( linspace(0,1,N)*1im )
-  p = NFFTPlan(x, N, m, sigma);
+D = 0
+for N in [(128,), (16,16), (12,12,12), (6,6,6,6)]
+  D += 1
+  @printf("Testing in dimension %u...\n", D)
 
-  f = ndft_adjoint(p, fHat)
-  fApprox = nfft_adjoint(p, fHat)
-  e = norm(f-fApprox) / norm(f)
-  println(e)
-  @test e < eps
-
-  gHat = ndft(p, f)
-  gHatApprox = nfft(p,f)
-
-  e = norm(gHat-gHatApprox) / norm(gHat) 
-  println(e)
-  @test e < eps
-end
-
-# 2D
-begin
-  N = (16,16)
   M = prod(N)
-  x = reshape(linspace(-0.4, 0.4, 2*M), 2, M)
+  x = reshape(linspace(-0.4, 0.4, D*M), D, M)
   if !(typeof(x) <: Array)
 	  x = collect(x)
   end
-  fHat = linspace(0,1,M)*1im
   p = NFFTPlan(x, N, m, sigma)
 
+  fHat = linspace(0,1,M)*im
   f = ndft_adjoint(p, fHat)
   fApprox = nfft_adjoint(p, fHat)
-  e = norm(f[:]-fApprox[:]) / norm(f[:])
+  e = norm(f[:] - fApprox[:]) / norm(f[:])
   println(e)
   @test e < eps
 
   gHat = ndft(p, f)
   gHatApprox = nfft(p, f)
-  e = norm(gHat[:]-gHatApprox[:]) / norm(gHat[:])
+  e = norm(gHat[:] - gHatApprox[:]) / norm(gHat[:])
   println(e)
   @test e < eps
 end
-
-# 3D
-begin
-  N = (12,12,12)
-  M = prod(N)
-  x = reshape(linspace(-0.4, 0.4, 3*M), 3, M)
-  if !(typeof(x) <: Array)
-	  x = collect(x)
-  end
-  fHat = linspace(0,1,M)*1im
-  p = NFFTPlan(x, N, m, sigma)
-
-  f = ndft_adjoint(p, fHat)
-  fApprox = nfft_adjoint(p, fHat)
-  e = norm(f[:]-fApprox[:]) / norm(f[:])
-  println(e)
-  @test e < eps
-
-  gHat = ndft(p, f)
-  gHatApprox = nfft(p, f)
-  e = norm(gHat[:]-gHatApprox[:]) / norm(gHat[:])
-  println(e)
-  @test e < eps
-end
-
 
