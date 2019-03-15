c520b080f51637d1e73ae52fe3bd102bf06bd88c
autodocs <autodocs>
2019-03-15 11:28:55 +0000

    build based on dece221

---
+++ dev/search_index.js
@@ -0,0 +1,107 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#NFFT.jl-1",
+    "page": "Home",
+    "title": "NFFT.jl",
+    "category": "section",
+    "text": "Julia package the Non-equidistant Fast Fourier Transform"
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Home",
+    "title": "Introduction",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "Start julia and open the package mode by entering ]. Then enteradd NFFTThis will install the packages NFFT.jl and all its dependencies."
+},
+
+{
+    "location": "index.html#License-/-Terms-of-Usage-1",
+    "page": "Home",
+    "title": "License / Terms of Usage",
+    "category": "section",
+    "text": "The source code of this project is licensed under the MIT license. This implies that you are free to use, share, and adapt it. However, please give appropriate credit by citing the project."
+},
+
+{
+    "location": "index.html#Contact-1",
+    "page": "Home",
+    "title": "Contact",
+    "category": "section",
+    "text": "If you have problems using the software, find mistakes, or have general questions please use the issue tracker to contact us."
+},
+
+{
+    "location": "index.html#Contributors-1",
+    "page": "Home",
+    "title": "Contributors",
+    "category": "section",
+    "text": "Tobias Knopp"
+},
+
+{
+    "location": "overview.html#",
+    "page": "Overview",
+    "title": "Overview",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "overview.html#Overview-1",
+    "page": "Overview",
+    "title": "Overview",
+    "category": "section",
+    "text": "TODO"
+},
+
+{
+    "location": "directional.html#",
+    "page": "Directional",
+    "title": "Directional",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "directional.html#Directional-NFFT-1",
+    "page": "Directional",
+    "title": "Directional NFFT",
+    "category": "section",
+    "text": "TODO"
+},
+
+{
+    "location": "density.html#",
+    "page": "Density",
+    "title": "Density",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "density.html#Sampling-Density-1",
+    "page": "Density",
+    "title": "Sampling Density",
+    "category": "section",
+    "text": "TODO"
+},
+
+]}
