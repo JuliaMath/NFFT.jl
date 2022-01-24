557b8bc0eea8d0dc766d224473adc8eb325a7aa4
Tobias <tobias@knoppweb.de>
2022-01-24 13:30:29 +0000

    new runs

--- benchmark/performance_threading.jl
+++ benchmark/performance_threading.jl
@@ -4,7 +4,7 @@ using Plots, StatsPlots, CategoricalArrays
 pgfplotsx()
 #gr()
 
-BenchmarkTools.DEFAULT_PARAMETERS.seconds = 10
+BenchmarkTools.DEFAULT_PARAMETERS.seconds = 40
 
 include("../NFFT3/NFFT3.jl")
 
