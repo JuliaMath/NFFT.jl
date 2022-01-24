68043578333607f34228d27f044e7473fc0cc518
Tobias Knopp <tobias.knopp@tuhh.de>
2022-01-24 21:48:47 +0100

    comment out NFFTTools until it is registered.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ using Test
 using NFFT
 using Random
 using CuNFFT
-using NFFTTools
+#using NFFTTools
 
 Random.seed!(123)
 
 include("constructors.jl")
 include("accuracy.jl")
 include("performance.jl")
-include("testToeplitz.jl")
-include("samplingDensity.jl")
+#include("testToeplitz.jl")
+#include("samplingDensity.jl")
