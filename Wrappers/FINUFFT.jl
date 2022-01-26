62ca8106378b12cd8b1ca411cccf2f161080dbe2
Tobias Knopp <tobias.knopp@tuhh.de>
2022-01-26 09:16:46 +0100

    Update FINUFFT.jl

--- Wrappers/FINUFFT.jl
+++ Wrappers/FINUFFT.jl
@@ -4,27 +4,29 @@ mutable struct FINUFFTPlan{T,D} <: AbstractNFFTPlan{T,D,1}
   N::NTuple{D,Int64}
   M::Int64
   x::Matrix{T}
-  tol::T
+  m::Int
+  σ::T
+  reltol::T
 end
 
 ################
 # constructors
 ################
 
-function FINUFFTPlan(x::Matrix{T}, N::NTuple{D,Int}; # m = 4, σ = 2.0, TODO
+function FINUFFTPlan(x::Matrix{T}, N::NTuple{D,Int}; 
               dims::Union{Integer,UnitRange{Int64}}=1:D,
               #fftflags=UInt32(NFFT3.FFTW_ESTIMATE), 
               kwargs...) where {D,T}
 
   if dims != 1:D
-    error("NFFT3 does not support directional plans!")
+    error("FINUFFT directional plans not yet implemented!")
   end
 
   M = size(x,2)
 
-  tol = 1e-9        # requested relative tolerance
+  m, σ, reltol = accuracyParams(kwargs...)
 
-  return FINUFFTPlan(N, M, x * 2π, tol)
+  return FINUFFTPlan(N, M, x * 2π, m, T(σ), reltol)
 end
 
 
@@ -38,7 +40,7 @@ AbstractNFFTs.size_out(p::FINUFFTPlan) = (Int(p.M),)
 function AbstractNFFTs.nfft!(p::FINUFFTPlan{T,1}, f::AbstractArray, fHat::StridedArray;
              verbose=false, timing::Union{Nothing,TimingStats} = nothing) where {T}
 
-  FINUFFT.nufft1d2!(p.x, fHat, -1, p.tol, f);
+  FINUFFT.nufft1d2!(p.x, fHat, -1, p.reltol, f);
 
   return fHat
 end
@@ -46,7 +48,7 @@ end
 function AbstractNFFTs.nfft_adjoint!(p::FINUFFTPlan{T,1}, fHat::AbstractArray, f::StridedArray;
                      verbose=false, timing::Union{Nothing,TimingStats} = nothing) where {T}
 
-  FINUFFT.nufft1d1!(p.x, fHat, 1, p.tol, f);
+  FINUFFT.nufft1d1!(p.x, fHat, 1, p.reltol, f);
 
   return f
 end
@@ -54,7 +56,7 @@ end
 function AbstractNFFTs.nfft!(p::FINUFFTPlan{T,2}, f::AbstractArray, fHat::StridedArray;
   verbose=false, timing::Union{Nothing,TimingStats} = nothing) where {T}
 
-  FINUFFT.nufft2d2!(p.x[1,:], p.x[2,:], fHat, -1, p.tol, f);
+  FINUFFT.nufft2d2!(p.x[1,:], p.x[2,:], fHat, -1, p.reltol, f);
 
   return fHat
 end
@@ -62,7 +64,7 @@ end
 function AbstractNFFTs.nfft_adjoint!(p::FINUFFTPlan{T,2}, fHat::AbstractArray, f::StridedArray;
           verbose=false, timing::Union{Nothing,TimingStats} = nothing) where {T}
 
-  FINUFFT.nufft2d1!(p.x[1,:], p.x[2,:], fHat, 1, p.tol, f);
+  FINUFFT.nufft2d1!(p.x[1,:], p.x[2,:], fHat, 1, p.reltol, f);
 
   return f
 end
@@ -70,7 +72,7 @@ end
 function AbstractNFFTs.nfft!(p::FINUFFTPlan{T,3}, f::AbstractArray, fHat::StridedArray;
   verbose=false, timing::Union{Nothing,TimingStats} = nothing) where {T}
 
-  FINUFFT.nufft3d2!(p.x[1,:], p.x[2,:], p.x[3,:], fHat, -1, p.tol, f);
+  FINUFFT.nufft3d2!(p.x[1,:], p.x[2,:], p.x[3,:], fHat, -1, p.reltol, f);
 
   return fHat
 end
@@ -78,7 +80,7 @@ end
 function AbstractNFFTs.nfft_adjoint!(p::FINUFFTPlan{T,3}, fHat::AbstractArray, f::StridedArray;
           verbose=false, timing::Union{Nothing,TimingStats} = nothing) where {T}
 
-  FINUFFT.nufft3d1!(p.x[1,:], p.x[2,:], p.x[3,:], fHat, 1, p.tol, f);
+  FINUFFT.nufft3d1!(p.x[1,:], p.x[2,:], p.x[3,:], fHat, 1, p.reltol, f);
 
   return f
 end
