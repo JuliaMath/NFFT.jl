2fe481ee2fa3911eb61a58cc5555809fb705e3f7
Tobias Knopp <tobias.knopp@tuhh.de>
2022-10-25 16:16:49 +0200

    quick fix for #106

--- src/precomputation.jl
+++ src/precomputation.jl
@@ -180,26 +180,32 @@ function precomputeOneNodeBlocking(winLin, winTensor::Nothing, winPoly::Nothing,
   return (y, tmpWin)
 end
 
-@generated function shiftedWindowEntries(winLin::Vector, idx, scale, d, L::Val{Z}) where {Z}
+@generated function shiftedWindowEntries(winLin::Vector, idx::T, scale, d, L::Val{Z}) where {T,Z}
   quote
-    idxL = floor(Int,idx)
-    idxInt = Int(idxL)
-    α = ( idx-idxL )
+    idxInt = floor(Int,idx)
+    α = ( idx-idxInt )
 
-    tmpWin = @ntuple $(Z) l -> begin
-      # Uncommented code: This is the version where we pull in l into the abs.
-      # We pulled this out of the iteration.
-      # idx = abs((kscale - (l-1)  - off)*LUTSize)/(m)
+    if α != zero(T) 
+      tmpWin = @ntuple $(Z) l -> begin
+        # Uncommented code: This is the version where we pull in l into the abs.
+        # We pulled this out of the iteration.
+        # idx = abs((kscale - (l-1)  - off)*LUTSize)/(m)
 
-      # The second +1 is because Julia has 1-based indexing
-      # The first +1 is part of the index calculation and needs(!)
-      # to be part of the abs. The abs is shifting to the positive interval
-      # and this +1 matches the `floor` above, which rounds down. In turn
-      # for positive and negative indices a different neighbour is calculated
-      idxInt1 = abs( idxInt - (l-1)*scale ) +1
-      idxInt2 = abs( idxInt - (l-1)*scale +1) +1
+        # The second +1 is because Julia has 1-based indexing
+        # The first +1 is part of the index calculation and needs(!)
+        # to be part of the abs. The abs is shifting to the positive interval
+        # and this +1 matches the `floor` above, which rounds down. In turn
+        # for positive and negative indices a different neighbour is calculated
+        idxInt1 = abs( idxInt - (l-1)*scale ) +1
+        idxInt2 = abs( idxInt - (l-1)*scale +1) +1
 
-      (winLin[idxInt1] + α * (winLin[idxInt2] - winLin[idxInt1]))
+        (winLin[idxInt1] + α * (winLin[idxInt2] - winLin[idxInt1]))
+      end
+    else
+      tmpWin = @ntuple $(Z) l -> begin
+        idxInt1 = abs( idxInt - (l-1)*scale ) + 1
+        winLin[idxInt1]
+      end
     end
     return tmpWin
   end
---
+++ test/issues.jl
@@ -0,0 +1,18 @@
+@testset "Issues Encountered during Development" begin
+
+  # https://github.com/JuliaMath/NFFT.jl/issues/106
+  # The issue was an out of bounce in the linear interpolation code.
+  T = Float32
+  Nz = 120
+  img_shape_os = (2Nz,)
+  λ = Array{Complex{T}}(undef, img_shape_os)
+  
+  trj = Matrix{T}(undef, 1, 2)
+  nfftplan = plan_nfft(trj, img_shape_os; precompute = LINEAR, blocking = false, fftflags = FFTW.ESTIMATE)
+  
+  trj[1,:] .= 0.008333333 # throws error
+  nfftplan = plan_nfft(trj, img_shape_os; precompute = LINEAR, blocking = false, fftflags = FFTW.ESTIMATE)
+  mul!(λ, adjoint(nfftplan), ones(Complex{T}, size(trj,2)))
+  
+
+end
