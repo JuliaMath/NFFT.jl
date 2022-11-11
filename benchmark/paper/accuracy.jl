7c182646af512915903beca8fdc6faa46fe543f4
Tobias Knopp <tobias.knopp@tuhh.de>
2022-11-11 08:49:26 +0100

    work on accuracy plot

--- benchmark/paper/accuracy.jl
+++ benchmark/paper/accuracy.jl
@@ -1,4 +1,4 @@
-using NFFT, DataFrames, LinearAlgebra, Statistics, LaTeXStrings, DelimitedFiles 
+using NFFT, DataFrames, LinearAlgebra, Statistics, LaTeXStrings, DelimitedFiles, Random
 using Plots; pgfplotsx()
 
 include("../../Wrappers/NFFT3.jl")
@@ -12,49 +12,52 @@ const blocking = [false, true, true, true, false, false, false]
 
 const σs = range(1.25, 4, length=12)
 const ms = 3:10
-const NBase = [4096, 64, 16]
+const NBase = [512*512, 512, 64]
 const Ds = 1:3
+const P = 1
 
-function nfft_accuracy_comparison(Ds, σs, ms)
+function nfft_accuracy_comparison(Ds, σs, ms, seed=2)
   println("\n\n ##### nfft_accuracy_comparison ##### \n\n")
 
   df = DataFrame(Package=String[], D=Int[], J=Int[], N=Int[], m = Int[], σ=Float64[],
                    ErrorTrafo=Float64[], ErrorAdjoint=Float64[] )  
 
-  for D in Ds
-      N = ntuple(d->NBase[D], D)
-      J = prod(N)
-      
-      for σ in σs
-        for m in ms
-          @info "m=$m D=$D σ=$σ "
-          k = rand(D,J) .- 0.5
-          fHat = randn(ComplexF64, J)
+  Random.seed!(seed)               
 
-          # ground truth (numerical)
-          pNDFT = NDFTPlan(k, N)
-          f = adjoint(pNDFT) * fHat
-          gHat = pNDFT * f
+  for p = 1:P
+    @info p
+    for D in Ds
+        N = ntuple(d->NBase[D], D)
+        J = prod(N)
+        
+        for σ in σs
+          for m in ms
+            @info "m=$m D=$D σ=$σ "
+            k = rand(D,J) .- 0.5
+            fHat = randn(ComplexF64, J)
+            f = rand(ComplexF64, N)
 
-          for pl = 1:length(packagesStr)
-            planner = packagesCtor[pl]
-            if planner != FINUFFT || σ == 2.0 # FINUFFT is not included in sigma sweep
-              p = planner(k, N; m, σ, precompute=precomp[pl], blocking=blocking[pl])
+            # ground truth (numerical)
+            #pNDFT = NDFTPlan(k, N)
+            pNDFT = NFFT3Plan(k, N; m=12, σ=2)
 
-              #if planner == CuNFFT.CuNFFTPlan
-              #  fApprox = Array(adjoint(p) * CuNFFT.CuArray(fHat))
-              #  gHatApprox = Array(p * CuNFFT.CuArray(f))
-              #else
-                fApprox = adjoint(p) * fHat
+            g = adjoint(pNDFT) * fHat
+            gHat = pNDFT * f
+
+            for pl = 1:length(packagesStr)
+              planner = packagesCtor[pl]
+              if planner != FINUFFT || σ == 2.0 # FINUFFT is not included in sigma sweep
+                p = planner(k, N; m, σ, precompute=precomp[pl], blocking=blocking[pl])
+
+                gApprox = adjoint(p) * fHat
                 gHatApprox = p * f
-              #end
 
-              eadjoint = norm(f[:] - fApprox[:], Inf) / norm(f[:], Inf)
-
-              etrafo = norm(gHat[:] - gHatApprox[:], Inf) / norm(gHat[:], Inf)
-              
-              push!(df, (packagesStr[pl], D, J, N[D], m, σ, etrafo, eadjoint))
-            end
+                eadjoint = norm(g[:] - gApprox[:], Inf) / norm(g[:], Inf)
+                etrafo = norm(gHat[:] - gHatApprox[:], Inf) / norm(gHat[:], Inf)
+                
+                push!(df, (packagesStr[pl], D, J, N[D], m, σ, etrafo, eadjoint))
+              end
+          end
         end
       end
     end
@@ -67,8 +70,10 @@ function plot_accuracy_m(df, packagesStr, packagesStrShort, filename, D=1,
 
   σs = range(1.25, 4, length=12)
 
-  df1_ = df[df.σ.==2.0 .&& df.D.==D,:]
-  #df2_ = df[df.m.==8 .&& df.D.==D,:]
+  df1 = df[df.σ.==2.0 .&& df.D.==D,:]
+  gdf = groupby(df1, Cols(:m, :Package))
+  df1_ = combine(gdf, :ErrorTrafo => mean => :ErrorTrafo, 
+                      :ErrorAdjoint => mean => :ErrorAdjoint)
 
   Plots.scalefontsizes()
   Plots.scalefontsizes(1.5)
@@ -80,7 +85,7 @@ function plot_accuracy_m(df, packagesStr, packagesStrShort, filename, D=1,
   p1 = plot(ms, df1_[df1_.Package.==packagesStr[1],:ErrorTrafo], 
             yscale = :log10, label=packagesStrShort[1], lw=2, xlabel = L"m", ylabel="Relative Error",
             legend = (:topright), title=L"\textrm{NFFT}", 
-            shape=shape[1], ls=ls[1], ylims = (1e-15,2e-5), xlims = (3,10),
+            shape=shape[1], ls=ls[1], ylims = (1e-15,4e-5), xlims = (3,10),
             c=colors[1], msc=colors[1], mc=colors[1])
 
   for p=2:length(packagesStr)      
@@ -92,7 +97,7 @@ function plot_accuracy_m(df, packagesStr, packagesStrShort, filename, D=1,
   p2 = plot(ms, df1_[df1_.Package.==packagesStr[1],:ErrorAdjoint], 
             yscale = :log10, lw=2, xlabel = L"m", #ylabel="Relative Error",
             legend = nothing, title=L"\textrm{NFFT}^H", 
-            shape=shape[1], ls=ls[1], ylims = (1e-15,2e-5), xlims = (3,10),
+            shape=shape[1], ls=ls[1], ylims = (1e-15,4e-5), xlims = (3,10),
             c=colors[1], msc=colors[1], mc=colors[1])
 
   for p=2:length(packagesStr)      
@@ -112,16 +117,11 @@ end
 
 
 
-#dfm = nfft_accuracy_comparison(2, [2.0], ms)
-#dfσ = nfft_accuracy_comparison(2, σs, [4])
-
-#writedlm("data/accuracy_m.csv", Iterators.flatten(([names(dfm)], eachrow(dfm))), ',')
-#writedlm("data/accuracy_sigma.csv", Iterators.flatten(([names(dfσ)], eachrow(dfσ))), ',')
+dfm = nfft_accuracy_comparison(2, [2.0], ms, 2)
+writedlm("data/accuracy_m.csv", Iterators.flatten(([names(dfm)], eachrow(dfm))), ',')
 
 data, header = readdlm("data/accuracy_m.csv", ',', header=true);
 dfm = DataFrame(data, vec(header))
-data, header = readdlm("data/accuracy_sigma.csv", ',', header=true);
-dfσ = DataFrame(data, vec(header))
 
 
 plot_accuracy_m(dfm, ["NFFT.jl/TENSOR", "NFFT3/TENSOR", "FINUFFT"],
@@ -133,10 +133,13 @@ plot_accuracy_m(dfm, ["NFFT.jl/FULL", "NFFT.jl/TENSOR", "NFFT.jl/LINEAR", "NFFT.
                       [RGB(0.7,0.13,0.16), RGB(0.3,0.5,0.7), RGB(0.5,0.48,0.45) ,RGB(0.0,0.29,0.57)])
 
 @info "Mean error deviation  NFFT.jl / NFFT3"
-mean((dfm[dfm.Package.=="NFFT.jl/POLY",:ErrorTrafo] ./ dfm[dfm.Package.=="NFFT3/TENSOR",:ErrorTrafo])[1:5])
+@info mean((dfm[dfm.Package.=="NFFT.jl/POLY",:ErrorTrafo] ./ dfm[dfm.Package.=="NFFT3/TENSOR",:ErrorTrafo])[1:5])
+@info mean((dfm[dfm.Package.=="NFFT.jl/POLY",:ErrorAdjoint] ./ dfm[dfm.Package.=="NFFT3/TENSOR",:ErrorAdjoint])[1:5])
+
 
 @info "Mean error deviation  FINUFFT / NFFT.jl"
-mean((dfm[dfm.Package.=="FINUFFT",:ErrorTrafo] ./ dfm[dfm.Package.=="NFFT.jl/POLY",:ErrorTrafo])[1:5])
+@info mean((dfm[dfm.Package.=="FINUFFT",:ErrorTrafo] ./ dfm[dfm.Package.=="NFFT.jl/POLY",:ErrorTrafo])[1:5])
+@info mean((dfm[dfm.Package.=="FINUFFT",:ErrorAdjoint] ./ dfm[dfm.Package.=="NFFT.jl/POLY",:ErrorAdjoint])[1:5])
 
 
 
