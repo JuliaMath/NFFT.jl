2ec58f8fd21b9d7852bb18edd53703fb46333b41
Jeff Fessler <fessler@umich.edu>
2026-01-02 17:35:20 -0500

    Use real weights with real convolve!

--- NFFTTools/src/samplingDensity.jl
+++ NFFTTools/src/samplingDensity.jl
@@ -17,7 +17,7 @@ end
 
 
 """
-   weights = sdc(plan::AbstractNFFTPlan; iters=20)
+   weights = sdc(plan::AbstractNFFTPlan; iters=20, weights)
 
 Compute weights for sample density compensation for given NFFT `plan`
 Uses method of Pipe & Menon, Mag Reson Med, 44(1):179-186, Jan. 1999.
@@ -28,32 +28,28 @@ from -0.5 to 0.5-1/N, then the returned weights are ≈ 1/N.
 
 The returned vector is real, positive values of length `plan.J`.
 """
-function sdc(p::AbstractNFFTPlan{T,D,1}; iters=20) where {T,D}
-    #=
-    We must initialize the weights to be a Complex vector of ones
-    because the `convolve!` methods require Complex arrays.
-    Nevertheless, the imaginary part of the weights
-    will be zero throughout this entire algorithm
-    because the interpolation kernel used in `convolve!` is real.
-    In fact the weights will remain strictly positive.
-    =#
-    weights = _fill_similar(p.tmpVec, one(Complex{T}), p.J)
-    weights_tmp = similar(weights)
+function sdc(
+    p::AbstractNFFTPlan{T,D,1};
+    weights::AbstractVector{T} = _fill_similar(p.tmpVec, one(T), p.J),
+    iters::Int = 20,
+    # the following are working buffers:
+    workg::AbstractArray = Array{real(eltype(p.tmpVec))}(undef, p.Ñ),
+    weights_tmp::AbstractVector = similar(weights),
+) where {T <: Real, D}
+
     scaling_factor = missing # will be set below
 
     # Pre-weighting to correct non-uniform sample density
     for i in 1:iters
-        convolve_transpose!(p, weights, p.tmpVec)
-        @assert all(x -> imag(x) == 0, p.tmpVec) # should be real!
+        convolve_transpose!(p, weights, workg)
         if i == 1
-            scaling_factor = maximum(real, p.tmpVec)
+            scaling_factor = maximum(workg)
         end
 
-        p.tmpVec ./= scaling_factor
-        convolve!(p, p.tmpVec, weights_tmp)
-        @assert all(z -> imag(z) == 0, weights_tmp) # should be real!
+        workg ./= scaling_factor
+        convolve!(p, workg, weights_tmp)
         weights_tmp ./= scaling_factor
-        any(z -> real(z) ≤ 0, weights_tmp) && throw("non-positive weights")
+        any(≤(0), weights_tmp) && throw("non-positive weights")
         @. weights /= (weights_tmp + eps(T))
     end
 
@@ -64,6 +60,7 @@ function sdc(p::AbstractNFFTPlan{T,D,1}; iters=20) where {T,D}
     u = _fill_similar(weights, one(Complex{T}), p.N)
 
 #=
+todo: can we cut this?
     # conversion to Array is a workaround for CuNFFT. Without it we get strange
     # results that indicate some synchronization issue
     f = Array( p * u )
@@ -80,10 +77,11 @@ function sdc(p::AbstractNFFTPlan{T,D,1}; iters=20) where {T,D}
     v = similar(p.tmpVec, Complex{T}, p.N)
     mul!(v, adjoint(p), f)
     c = vec(v) \ vec(u)  # least squares diff
-    @assert c ≈ real(c) # should be real!
+@assert c ≈ real(c) # should be real!
+#@assert c == real(c) # should be real!
     c = real(c)
+@assert c > 0
 # todo: solve directly for real c after checking the math
 
-#   return abs.(c * weights)
-    return c * real(weights)
+    return c * weights
 end
