b532dcc4f9913b72da8cfb36730f4e3f505ef2bc
Jeff Fessler <fessler@umich.edu>
2026-01-02 17:38:22 -0500

    Odd-sized test

--- test/samplingDensity.jl
+++ test/samplingDensity.jl
@@ -8,11 +8,11 @@ using Test: @test, @testset
 
 @testset "Sampling Density" begin
 
-# create a 10×8 grid of unit spaced sampling points
-N = (10, 8)
+# create a 9×8 grid of unit spaced sampling points
+N = (9, 8)
 T = Float32
-g = N -> @. T((0:(N-1)) / N - 0.5)
-nodes = hcat([[x; y] for x in g(N[1]), y in g(N[2])]...)
+fun = N -> @. T((0:(N-1)) / N - 0.5) # 1D grid
+nodes = hcat([[x; y] for x in fun(N[1]), y in fun(N[2])]...)
 
 for pre in [NFFT.LINEAR, NFFT.FULL, NFFT.TENSOR, NFFT.POLYNOMIAL]
   # approximate the density weights
@@ -20,7 +20,7 @@ for pre in [NFFT.LINEAR, NFFT.FULL, NFFT.TENSOR, NFFT.POLYNOMIAL]
   weights = sdc(p, iters = 10)
 
   @test T == eltype(weights)
-  @test all(isreal, weights)
+  @test isreal(weights)
   @test all(>(0), weights)
   @test all(≈(1/prod(N)), weights)
 end
