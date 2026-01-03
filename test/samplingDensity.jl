e953b338aa045f224b99a7435d7169eb3f25e3a1
Jeff Fessler <fessler@umich.edu>
2026-01-03 09:18:10 -0500

    Try to test non-allocation

--- test/samplingDensity.jl
+++ test/samplingDensity.jl
@@ -4,6 +4,7 @@ Test sampling density compensation
 using NFFT: plan_nfft
 using NFFTTools: sdc
 import NFFT # LINEAR, FULL, TENSOR, POLYNOMIAL
+import NFFTTools # sdc!
 using Test: @test, @testset
 
 @testset "Sampling Density" begin
@@ -17,12 +18,43 @@ nodes = hcat([[x; y] for x in fun(N[1]), y in fun(N[2])]...)
 for pre in [NFFT.LINEAR, NFFT.FULL, NFFT.TENSOR, NFFT.POLYNOMIAL]
   # approximate the density weights
   p = plan_nfft(nodes, N, m = 5, σ = 2.0, precompute=pre)
-  weights = sdc(p, iters = 10)
+  @show @allocated weights = sdc(p; iters = 10)
 
   @test T == eltype(weights)
   @test isreal(weights)
   @test all(>(0), weights)
   @test all(≈(1/prod(N)), weights)
+
+  # Test the version with provided work buffers
+  w2 = deepcopy(weights)
+  fill!(w2, one(T))
+  sdc(p; iters = 10,
+      weights = w2,
+      weights_tmp = similar(w2),
+      workg = Array{real(eltype(p.tmpVec))}(undef, p.Ñ),
+      workf = similar(p.tmpVec, Complex{T}, p.J),
+      workv = similar(p.tmpVec, Complex{T}, p.N),
+  )
+  @test w2 == weights
+
+  # pre-allocate
+  weights_tmp = similar(w2)
+  workg = Array{real(eltype(p.tmpVec))}(undef, p.Ñ)
+  workf = similar(p.tmpVec, Complex{T}, p.J)
+  workv = similar(p.tmpVec, Complex{T}, p.N)
+  fill!(w2, one(T))
+
+# ideally the following test should be non-allocating!?
+# @test 0 == @allocated
+  @show @allocated NFFTTools.sdc!(p, 10,
+      w2,
+      weights_tmp,
+      workg,
+      workf,
+      workv,
+  )
+  @test w2 == weights
+
 end
 
 end # @testset
