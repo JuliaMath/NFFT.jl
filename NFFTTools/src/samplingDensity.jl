34b62f6197c148731a9d46e060b980f8f0a3e9bb
Jeff Fessler <fessler@umich.edu>
2026-01-03 09:17:51 -0500

    Try to make it non-allocating

--- NFFTTools/src/samplingDensity.jl
+++ NFFTTools/src/samplingDensity.jl
@@ -15,26 +15,73 @@ function _fill_similar(array::AbstractArray, v::T, dims::Union{Integer,Dims}) wh
     return weights
 end
 
+#=
+It would be desirable to use the memory pointed to by p.tmpVec
+as working buffer for sdc iterations,
+but this attempt led to intermittent corrupted outputs and errors.
+=#
+function _reinterpret_real(g::StridedArray{Complex{T}}) where {T <: Real}
+    r1 = reinterpret(T, g)
+    r2 = @view r1[1:2:end,:]
+    return r2
+end
+
 
 """
-   weights = sdc(plan::AbstractNFFTPlan; iters=20, weights)
+   weights = sdc(plan::AbstractNFFTPlan; iters=20, ...)
 
 Compute weights for sample density compensation for given NFFT `plan`
 Uses method of Pipe & Menon, Mag Reson Med, 44(1):179-186, Jan. 1999.
 DOI: 10.1002/(SICI)1522-2594(199901)41:1<179::AID-MRM25>3.0.CO;2-V
+The function applies `iters` iterations of that method.
 
 The scaling here such that if the plan were 1D with N nodes equispaced
 from -0.5 to 0.5-1/N, then the returned weights are ≈ 1/N.
 
+The weights are scaled such that ``A' diag(w) A 1_N ≈ 1_N``.
+
 The returned vector is real, positive values of length `plan.J`.
+
+There are several named keyword arguments that are work buffers
+that are all mutated: `weights, workg weights_tmp workf workv`.
+If the caller provides all of those,
+then this function ideally should be non-allocating.
+todo: tests show that it still allocates in practice; unsure why
 """
 function sdc(
     p::AbstractNFFTPlan{T,D,1};
-    weights::AbstractVector{T} = _fill_similar(p.tmpVec, one(T), p.J),
     iters::Int = 20,
-    # the following are working buffers:
-    workg::AbstractArray = Array{real(eltype(p.tmpVec))}(undef, p.Ñ),
+    # the following are working buffers that are all mutated:
+    weights::AbstractVector{T} = _fill_similar(p.tmpVec, one(T), p.J),
     weights_tmp::AbstractVector = similar(weights),
+#   workg::AbstractArray = _reinterpret_real(p.tmpVec), # todo
+    workg::AbstractArray = Array{real(eltype(p.tmpVec))}(undef, p.Ñ),
+    workf::AbstractVector = similar(p.tmpVec, Complex{T}, p.J),
+    workv::AbstractArray = similar(p.tmpVec, Complex{T}, p.N),
+) where {T <: Real, D}
+
+  return sdc!(
+    p,
+    iters,
+    weights,
+    weights_tmp,
+    workg,
+    workf,
+    workv,
+  )
+end
+
+
+# ideally this function should be non-allocating
+function sdc!(
+    p::AbstractNFFTPlan{T,D,1},
+    iters::Int,
+    # the following are working buffers that are all mutated:
+    weights::AbstractVector{T},
+    weights_tmp::AbstractVector,
+    workg::AbstractArray,
+    workf::AbstractVector,
+    workv::AbstractArray,
 ) where {T <: Real, D}
 
     scaling_factor = missing # will be set below
@@ -50,14 +97,16 @@ function sdc(
         convolve!(p, workg, weights_tmp)
         weights_tmp ./= scaling_factor
         any(≤(0), weights_tmp) && throw("non-positive weights")
-        @. weights /= (weights_tmp + eps(T))
+        weights_tmp .+= eps(T) # todo: might not be necessary?
+        weights ./= weights_tmp
     end
 
-    # Post weights to correct image scaling
-    # This finds c, where ‖u - c * v‖₂² = 0 and then uses
-    # c to scale all weights by a scalar factor.
-
-    u = _fill_similar(weights, one(Complex{T}), p.N)
+    #=
+    We want to scale the weights such that A' D(w) A 1_N ≈ 1_N.
+    We find c ∈ ℝ that minimizes ‖u - c * v‖₂ where u ≜ 1_N
+    and v ≜ A' D(w) A 1_N, and then scale w by that c.
+    The analytical solution is c = real(u'v) / ‖v‖² = real(sum(v)) / ‖v‖².
+    =#
 
 #=
 todo: can we cut this?
@@ -71,17 +120,13 @@ todo: can we cut this?
 =#
 
     # non converting version
-    f = similar(p.tmpVec, Complex{T}, p.J)
-    mul!(f, p, u)
-    f .*= weights # apply weights from above
-    v = similar(p.tmpVec, Complex{T}, p.N)
-    mul!(v, adjoint(p), f)
-    c = vec(v) \ vec(u)  # least squares diff
-@assert c ≈ real(c) # should be real!
-#@assert c == real(c) # should be real!
-    c = real(c)
-@assert c > 0
-# todo: solve directly for real c after checking the math
+    u = workv # trick to save memory
+    fill!(u, one(T))
+    mul!(workf, p, u)
+    workf .*= weights # apply weights from above
+    mul!(workv, adjoint(p), workf)
+    c = real(sum(workv)) / sum(abs2, workv)
 
-    return c * weights
+    weights .*= c
+    return weights
 end
