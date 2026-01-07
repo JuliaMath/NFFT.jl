557e84dd9c39c6836e0e148528ff1fe776402c14
Jeff Fessler <JeffFessler@users.noreply.github.com>
2026-01-07 13:33:21 -0500

    Fix? imports - untested WIP

--- NFFTTools/src/NFFTTools.jl
+++ NFFTTools/src/NFFTTools.jl
@@ -3,9 +3,9 @@ module NFFTTools
 export sdc
 export calculateToeplitzKernel, calculateToeplitzKernel!, convolveToeplitzKernel!
 
-#using AbstractNFFTs: AbstractNFFTPlan, plan_nfft, nodes! # someday...
-#using AbstractNFFTs: convolve!, convolve_transpose!
-using NFFT: NFFTPlan, plan_nfft, nodes! # current
+using AbstractNFFTs: AbstractNFFTPlan, plan_nfft, nodes!
+#using AbstractNFFTs: convolve!, convolve_transpose! # someday
+using NFFT: NFFTPlan
 using NFFT: convolve!, convolve_transpose!
 using FFTW: fftshift, plan_fft, plan_ifft
 using LinearAlgebra: adjoint, mul!
